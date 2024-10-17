13a82ad886667a603f34dc9fbf14929906fc2d64
Andrej Nenadić <andrejnana445@gmail.com>
2024-10-17 12:13:53 +0200

    Add 2 numbers on the gpu

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 (async () => {
   if (!("gpu" in navigator)) {
-    console.log("WebGPU is not supported.");
+    console.log(
+      "WebGPU is not supported. Enable chrome://flags/#enable-unsafe-webgpu flag."
+    );
     return;
   }
 
@@ -11,44 +13,147 @@
   }
   const device = await adapter.requestDevice();
 
-  // Get a GPU buffer in a mapped state and an arrayBuffer for writing.
-  const gpuWriteBuffer = device.createBuffer({
+  // First Matrix
+
+  const gpuBufferFirstMatrix = device.createBuffer({
     mappedAtCreation: true,
     size: 4,
-    usage: GPUBufferUsage.MAP_WRITE | GPUBufferUsage.COPY_SRC,
+    usage: GPUBufferUsage.STORAGE,
   });
-  const arrayBuffer = gpuWriteBuffer.getMappedRange();
+  const arrayBufferFirstMatrix = gpuBufferFirstMatrix.getMappedRange();
+  new Float32Array(arrayBufferFirstMatrix).set([1]);
+  gpuBufferFirstMatrix.unmap();
 
-  // Write bytes to buffer.
-  new Uint8Array(arrayBuffer).set([0, 1, 2, 3]);
+  // Second Matrix
 
-  // Unmap buffer so that it can be used later for copy.
-  gpuWriteBuffer.unmap();
+  const gpuBufferSecondMatrix = device.createBuffer({
+    mappedAtCreation: true,
+    size: 4,
+    usage: GPUBufferUsage.STORAGE,
+  });
+  const arrayBufferSecondMatrix = gpuBufferSecondMatrix.getMappedRange();
+  new Float32Array(arrayBufferSecondMatrix).set([2]);
+  gpuBufferSecondMatrix.unmap();
+
+  // Result Matrix
+
+  const resultMatrixBuffer = device.createBuffer({
+    size: 4,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
+  });
+
+  // Bind group layout and bind group
+
+  const bindGroupLayout = device.createBindGroupLayout({
+    entries: [
+      {
+        binding: 0,
+        visibility: GPUShaderStage.COMPUTE,
+        buffer: {
+          type: "read-only-storage",
+        },
+      },
+      {
+        binding: 1,
+        visibility: GPUShaderStage.COMPUTE,
+        buffer: {
+          type: "read-only-storage",
+        },
+      },
+      {
+        binding: 2,
+        visibility: GPUShaderStage.COMPUTE,
+        buffer: {
+          type: "storage",
+        },
+      },
+    ],
+  });
+
+  const bindGroup = device.createBindGroup({
+    layout: bindGroupLayout,
+    entries: [
+      {
+        binding: 0,
+        resource: {
+          buffer: gpuBufferFirstMatrix,
+        },
+      },
+      {
+        binding: 1,
+        resource: {
+          buffer: gpuBufferSecondMatrix,
+        },
+      },
+      {
+        binding: 2,
+        resource: {
+          buffer: resultMatrixBuffer,
+        },
+      },
+    ],
+  });
+
+  // Compute shader code
+
+  const shaderModule = device.createShaderModule({
+    code: `
+      @group(0) @binding(0) var<storage, read> first : f32;
+      @group(0) @binding(1) var<storage, read> second : f32;
+      @group(0) @binding(2) var<storage, read_write> result : f32;
+
+      @compute @workgroup_size(1)
+      fn main(@builtin(global_invocation_id) global_id : vec3<u32>) {
+        result = first + second;
+      }
+    `,
+  });
+
+  // Pipeline setup
+
+  const computePipeline = device.createComputePipeline({
+    layout: device.createPipelineLayout({
+      bindGroupLayouts: [bindGroupLayout],
+    }),
+    compute: {
+      module: shaderModule,
+      entryPoint: "main",
+    },
+  });
+
+  // Commands submission
+
+  const commandEncoder = device.createCommandEncoder();
+
+  const passEncoder = commandEncoder.beginComputePass();
+  passEncoder.setPipeline(computePipeline);
+  passEncoder.setBindGroup(0, bindGroup);
+  const workgroupCountX = 1;
+  const workgroupCountY = 1;
+  passEncoder.dispatchWorkgroups(workgroupCountX, workgroupCountY);
+  passEncoder.end();
 
   // Get a GPU buffer for reading in an unmapped state.
   const gpuReadBuffer = device.createBuffer({
-    mappedAtCreation: false,
     size: 4,
     usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ,
   });
 
   // Encode commands for copying buffer to buffer.
-  const copyEncoder = device.createCommandEncoder();
-  copyEncoder.copyBufferToBuffer(
-    gpuWriteBuffer /* source buffer */,
+  commandEncoder.copyBufferToBuffer(
+    resultMatrixBuffer /* source buffer */,
     0 /* source offset */,
     gpuReadBuffer /* destination buffer */,
     0 /* destination offset */,
     4 /* size */
   );
 
-  // Submit copy commands.
-  const copyCommands = copyEncoder.finish();
-  device.queue.submit([copyCommands]);
+  // Submit GPU commands.
+  const gpuCommands = commandEncoder.finish();
+  device.queue.submit([gpuCommands]);
 
   // Read buffer.
   await gpuReadBuffer.mapAsync(GPUMapMode.READ);
-  const copyArrayBuffer = gpuReadBuffer.getMappedRange();
-
-  console.log(new Uint8Array(copyArrayBuffer));
+  const arrayBuffer = gpuReadBuffer.getMappedRange();
+  console.log(new Float32Array(arrayBuffer));
 })();
